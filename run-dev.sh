5565d7c49e60f9797d7757ddb26f31067683d77f
Vicens Juan Tomas Montserrat <vicens.tomas@logitravel.com>
2017-11-14 14:43:21 +0100

    Fix run-dev.sh

--- run-dev.sh
+++ run-dev.sh
@@ -7,6 +7,6 @@ mkdir -p /tmp/mgob/data
 mkdir -p /tmp/mgob/storage
 mkdir -p /tmp/mgob/tmp
 
-cp test/config/mongo-dev.yml /tmp/mgob/config/
+cp test/config/mongo-local.yml /tmp/mgob/config/
 
 mgob -ConfigPath /tmp/mgob/config/ -DataPath /tmp/mgob/data/ -StoragePath /tmp/mgob/storage -TmpPath /tmp/mgob/tmp/
